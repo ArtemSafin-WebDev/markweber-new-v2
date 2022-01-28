2347183a85181c9a1e305bec9f8bff66869d1578
Artem Safin <artemsafin.webdev@gmail.com>
2022-01-28 12:32:15 +0300

    Тягание картинки

--- src/js/introSliderDrag.js
+++ src/js/introSliderDrag.js
@@ -25,4 +25,67 @@ export default function introSliderDrag() {
             });
         });
     });
+
+    const imageWrappers = Array.from(document.querySelectorAll('.intro__slider-image-wrapper'));
+
+    imageWrappers.forEach(wrapper => {
+        let hovered = false;
+        const image = wrapper.querySelector('.intro__slider-image');
+        const hoverArea = wrapper.querySelector('.intro__slider-image-hover-area');
+
+        let imageTop = image.getBoundingClientRect().top;
+        let imageLeft = image.getBoundingClientRect().left;
+
+        window.addEventListener('resize', () => {
+            imageTop = image.getBoundingClientRect().top;
+            imageLeft = image.getBoundingClientRect().left;
+        })
+        hoverArea.addEventListener('mouseenter', () => {
+            hovered = true;
+            console.log('hovered');
+        });
+        hoverArea.addEventListener('mouseleave', () => {
+            hovered = false;
+            console.log('not hovered');
+
+            // image.style.transform = ``;
+
+            gsap.to(image, {
+                x: 0,
+                y: 0,
+
+                duration: 0.3,
+                overwrite: true,
+                onComplete: () => {
+                    imageTop = image.getBoundingClientRect().top;
+                    imageLeft = image.getBoundingClientRect().left;
+                }
+            });
+        });
+
+        document.addEventListener('mousemove', e => {
+            if (hovered) {
+                const x = e.clientX;
+                const y = e.clientY;
+
+                const xOffset = x - imageLeft - image.offsetWidth / 2;
+                const yOffset = y - imageTop - image.offsetHeight / 2;
+
+                console.log('Y offset', yOffset);
+                console.log('X offset', xOffset);
+
+                // image.style.transform = `translateX(${xOffset}px) translateY(${yOffset}px)`;
+
+              
+
+                gsap.to(image, {
+                    x: xOffset * 0.3,
+                    y: yOffset * 0.3,
+
+                    duration: 0.3,
+                    overwrite: true
+                });
+            }
+        });
+    });
 }
